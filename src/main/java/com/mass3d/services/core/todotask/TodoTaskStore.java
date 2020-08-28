fda73275d65a46a9d57f2e175451e2e93aba12c1
Hamza-ye <7amza.it@gmail.com>
2020-08-28 19:12:59 +0300

    Add Objects Stores to each ready object in eagleboard

---
+++ src/main/java/com/mass3d/services/core/todotask/TodoTaskStore.java
@@ -0,0 +1,8 @@
+package com.mass3d.services.core.todotask;
+
+import com.mass3d.api.common.IdentifiableObjectStore;
+import com.mass3d.api.datafield.DataField;
+
+public interface TodoTaskStore extends IdentifiableObjectStore<DataField> {
+
+}
