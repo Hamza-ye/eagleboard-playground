5662351d6d265d0a245ef48acd25caf7a493a7ce
Hamza-ye <7amza.it@gmail.com>
2020-08-28 19:12:59 +0300

    Add Objects Stores to each ready object in eagleboard

---
+++ src/main/java/com/mass3d/services/core/fieldset/FieldSetStore.java
@@ -0,0 +1,8 @@
+package com.mass3d.services.core.fieldset;
+
+import com.mass3d.api.common.IdentifiableObjectStore;
+import com.mass3d.api.datafield.DataField;
+
+public interface FieldSetStore extends IdentifiableObjectStore<DataField> {
+
+}
---
+++ src/main/java/com/mass3d/services/core/project/ProjectStore.java
@@ -0,0 +1,8 @@
+package com.mass3d.services.core.project;
+
+import com.mass3d.api.common.IdentifiableObjectStore;
+import com.mass3d.api.datafield.DataField;
+
+public interface ProjectStore extends IdentifiableObjectStore<DataField> {
+
+}
