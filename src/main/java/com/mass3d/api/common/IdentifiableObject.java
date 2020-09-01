5830af884c7e0d21263b6a37077af4dac943e2c6
Hamza-ye <7amza.it@gmail.com>
2020-09-01 19:44:57 +0300

    Add user and usergroup

--- src/main/java/com/mass3d/api/common/IdentifiableObject.java
+++ src/main/java/com/mass3d/api/common/IdentifiableObject.java
@@ -1,7 +1,12 @@
 package com.mass3d.api.common;
 
+import com.mass3d.api.security.acl.Access;
+import com.mass3d.api.user.User;
+import com.mass3d.api.user.UserAccess;
+import com.mass3d.api.user.UserGroupAccess;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 
 
 public interface IdentifiableObject
@@ -23,9 +28,15 @@ public interface IdentifiableObject
 
   String getPropertyValue(IdScheme idScheme);
 
+  Set<UserGroupAccess> getUserGroupAccesses();
+
+  Set<UserAccess> getUserAccesses();
+
+  Access getAccess();
+
   String getPublicAccess();
 
   boolean getExternalAccess();
 
-//  User getUser();
+  User getUser();
 }
