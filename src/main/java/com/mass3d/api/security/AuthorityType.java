a0e4fc07b047b23952c55250fc52efa9371fb5b7
Hamza-ye <7amza.it@gmail.com>
2020-09-02 20:22:56 +0300

    Add Authorities and access support

---
+++ src/main/java/com/mass3d/api/security/AuthorityType.java
@@ -0,0 +1,13 @@
+package com.mass3d.api.security;
+
+public enum AuthorityType {
+  CREATE,
+  DATA_CREATE,
+  CREATE_PUBLIC,
+  CREATE_PRIVATE,
+  EXTERNALIZE,
+  READ,
+  DATA_READ,
+  UPDATE,
+  DELETE
+}
